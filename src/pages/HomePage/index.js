256ac1e76d0d8cdc832573fcff5610471f5a895e
SuperDev0403 <viclory0101@gmail.com>
2021-05-11 15:42:23 +0100

    update

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -20,7 +20,24 @@ class HomePage extends Component {
         <Header />
         <div className="container">
           <div className="mt-4 mb-5">
-            <h1>Welcome info</h1>
+            <div id="bodyDIV" class="flex">
+              <div id="bodyText">
+                <h1> Welcome to POENexus </h1>
+                <p>
+                  {" "}
+                  This site was designed to facilitate a safer, more
+                  organizable, searchable, database driven version of the trade
+                  functionality you find in the Discord-based trade channels.
+                </p>
+                <p>
+                  {" "}
+                  To begin using the system, please register using the form
+                  below. Once submitted, an email will be sent to the address
+                  you registered with and will contain a link to authorize your
+                  account.
+                </p>
+              </div>
+            </div>
           </div>
           <Register />
         </div>
