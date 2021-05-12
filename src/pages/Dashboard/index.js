ffa9cd40ac73b5f9d0cb7df09439a668798c9cda
SuperDev0403 <viclory0101@gmail.com>
2021-05-12 16:04:04 +0100

    update

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,11 +2,26 @@ import React, { Component } from "react";
 import Header from "../../components/Header";
 import MenuBar from "../../components/MenuBar";
 
+import { Button, InputGroup, FormControl, Spinner } from "react-bootstrap";
+
 class Dashboard extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      accountName: "",
+      email: "",
+      irlName: "",
+      discordId: "",
+      ign1: "",
+      ign2: "",
+      ign3: "",
+      loadingFlag: false,
+    };
   }
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
   render() {
     return (
@@ -16,6 +31,149 @@ class Dashboard extends Component {
         <div className="container">
           <div className="mt-4 mb-5">
             <h1>Dashboard</h1>
+            <div className="row mt-5">
+              <div className="col-md-6">
+                <h2>User Info</h2>
+                <form onSubmit={this.handleSubmit} className="dashboard-form">
+                  <div className="form-group mt-4 row">
+                    <label className="mt-2 col-md-4">Account Name:</label>
+                    <div className="col-md-6">
+                      <InputGroup>
+                        <FormControl
+                          placeholder="Account Name"
+                          value={this.state.accountName}
+                          name="accountName"
+                          onChange={this.handleChange}
+                          required
+                        />
+                      </InputGroup>
+                      <p>
+                        <span className="text-danger">*</span> requires admin
+                        approval
+                      </p>
+                    </div>
+                  </div>
+                  <div className="form-group row">
+                    <label className="mt-2 col-md-4">Email:</label>
+                    <div className="col-md-6">
+                      <InputGroup>
+                        <FormControl
+                          type="email"
+                          placeholder="Email"
+                          value={this.state.email}
+                          name="email"
+                          onChange={this.handleChange}
+                          required
+                        />
+                      </InputGroup>
+                      <p>
+                        <span className="text-danger">*</span> requires admin
+                        approval
+                      </p>
+                    </div>
+                  </div>
+                  <div className="form-group row mb-3">
+                    <label className="mt-2 col-md-4">IRL Name:</label>
+                    <div className="col-md-6">
+                      <InputGroup>
+                        <FormControl
+                          placeholder="IRL Name"
+                          value={this.state.irlName}
+                          name="irlName"
+                          onChange={this.handleChange}
+                          required
+                        />
+                      </InputGroup>
+                    </div>
+                  </div>
+                  <div className="form-group row">
+                    <label className="mt-2 col-md-4">Discord ID:</label>
+                    <div className="col-md-6">
+                      <InputGroup>
+                        <FormControl
+                          placeholder="Discord ID"
+                          value={this.state.discordId}
+                          name="discordId"
+                          onChange={this.handleChange}
+                          required
+                        />
+                      </InputGroup>
+                    </div>
+                  </div>
+                  <h2 className="mt-4">IGNs</h2>
+                  <div className="form-group row mb-3">
+                    <label className="mt-2 col-md-4">IGN 1:</label>
+                    <div className="col-md-6">
+                      <InputGroup>
+                        <FormControl
+                          placeholder="IGN 1"
+                          value={this.state.ign1}
+                          name="ign1"
+                          onChange={this.handleChange}
+                          required
+                        />
+                      </InputGroup>
+                    </div>
+                  </div>
+                  <div className="form-group row mb-3">
+                    <label className="mt-2 col-md-4">IGN 2:</label>
+                    <div className="col-md-6">
+                      <InputGroup>
+                        <FormControl
+                          placeholder="IGN 2"
+                          value={this.state.ign2}
+                          name="ign2"
+                          onChange={this.handleChange}
+                          required
+                        />
+                      </InputGroup>
+                    </div>
+                  </div>
+                  <div className="form-group row mb-3">
+                    <label className="mt-2 col-md-4">IGN 3:</label>
+                    <div className="col-md-6">
+                      <InputGroup>
+                        <FormControl
+                          placeholder="IGN 3"
+                          value={this.state.ign3}
+                          name="ign3"
+                          onChange={this.handleChange}
+                          required
+                        />
+                      </InputGroup>
+                    </div>
+                  </div>
+                  <div>
+                    <Button
+                      variant="success"
+                      type="submit"
+                      className="w-25 mt-3 mb-5"
+                    >
+                      SAVE
+                      {this.state.loadingFlag === true ? (
+                        <Spinner animation="border" />
+                      ) : (
+                        ""
+                      )}
+                    </Button>
+                  </div>
+                </form>
+              </div>
+              <div className="col-md-6">
+                <h2>Tier X</h2>
+                <div className="mt-4">
+                  <p>Trader Rating : XXXX</p>
+                  <p>___</p>
+                </div>
+              </div>
+            </div>
+            <div>
+              <h2>Active Sell Listings</h2>
+              <div className="mt-4">
+                <p>1.</p>
+                <p>1.</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/pages/Security/index.js
+++ src/pages/Security/index.js
@@ -2,6 +2,13 @@ import React, { Component } from "react";
 import Header from "../../components/Header";
 import MenuBar from "../../components/MenuBar";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGreaterThanEqual,
+  faLessThan,
+  faLessThanEqual,
+} from "@fortawesome/free-solid-svg-icons";
+
 class Security extends Component {
   constructor(props) {
     super(props);
@@ -16,6 +23,87 @@ class Security extends Component {
         <div className="container">
           <div className="mt-4 mb-5">
             <h1>Security</h1>
+            <div>
+              <p className="mt-4">
+                POEnexus will be offering 3 types of trade security to begin
+                with:
+              </p>
+              <ul>
+                <li>Trader Rating</li>
+                <li>Unsecured</li>
+                <li>Escrow</li>
+              </ul>
+              <h3>Trader Rating:</h3>
+              <p className="mt-3">
+                Is the rating system built into the site trade system. All
+                trades will garner trader-rating points for any user of the
+                site. <br />
+                User ratings will be visible at time of purchase, and positive
+                feedback from a trade will be automatically added to a user,
+                once a trade is confirmed. <br />
+                As users accumulate positive trader-rating points, they qualify
+                for Trader Rep Tiers tiers:
+              </p>
+              <ul>
+                <li>
+                  Corrupted: denotes a trader rating{" "}
+                  <FontAwesomeIcon icon={faLessThan} className="mr-2" /> 0 Avoid
+                  trading with these individuals. Collateral Escrow Multiplier:
+                  0
+                </li>
+                <li>
+                  Tier I: denotes a trader rating{" "}
+                  <FontAwesomeIcon icon={faLessThanEqual} /> 2499. All new users
+                  start at this tier. Collateral Escrow Multiplier:: 1
+                </li>
+                <li>
+                  Tier II: denotes a trader rating 2500 to 4999. This user has
+                  performed dozens of successful trades using the system.
+                  Collateral Escrow Multiplier:: 1.5
+                </li>
+                <li>
+                  Tier III: denotes a trader rating 5000 to 9999. These users
+                  are avid, trustworthy traders on the platform. Collateral
+                  Escrow Multiplier: 2.0
+                </li>
+                <li>
+                  Tier IV: denotes a trader rating{" "}
+                  <FontAwesomeIcon icon={faGreaterThanEqual} /> 10000. These are
+                  the bet traders on the platform, and have garnered a massive
+                  amount of positive trades. Collateral Escrow Multiplier: 3.0
+                  *requires Verified status*
+                </li>
+              </ul>
+              <h3>Unsecured Trades:</h3>
+              <p className="mt-3">
+                Will be trades with no required form of trade security;
+                generally used for services that don't require the buyer to hand
+                over expensive items for the service to be performed.
+              </p>
+              <h3>Collateral Escrow System:</h3>
+              <p className="mt-3">
+                Unique to POEnexus will be our Trade Collateral Escrow system.
+                This will provide a much needed means of security for high-value
+                item services, allowing users to search for service posts from
+                user that have provided backing currency and comfortably hand
+                over their items without fear of losing the value of said item
+                to a scam. Users electing to utilize the Escrow system will be
+                able to deposit in-game currency with a POEnexus administrator.{" "}
+                <br />
+                Said deposit amount will be held for a selected amount of time,
+                either 3 or 7 days, after which, it will be returned to said
+                player, less a % service fee.
+                <br />
+                Current Fee rates are: 10% (of deposited amount) for 3 day
+                escrow, or 5% (of deposited amount) for 7 day escrow, based on
+                converted Chaos Orb rate from{" "}
+                <a href="https://POE.NINJA">
+                  <span>https://POE.NINJA</span>
+                </a>{" "}
+                at time of deposit return.
+                <br />
+              </p>
+            </div>
           </div>
         </div>
       </div>
